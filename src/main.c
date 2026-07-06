1208d04d5ec5f9abd479e3ece6eec5eab0298092
Joshua van Saarloos Vinkhuyzen <joshua.vinkhuyzen@gmail.com>
2026-07-06 14:17:39 +0200

    add clicking on a square will create a new one with a random velocity

--- src/main.c
+++ src/main.c
@@ -1,10 +1,29 @@
 #include "raylib.h"
 #include "win32_helper.h"
+#include <math.h>
+#include <time.h>
+
+#define MAX_SQUARES 64
+#define SQUARE_SIZE 50
+
+typedef struct {
+    Rectangle rect;
+    Vector2 velocity;
+} BouncingSquare;
+
+static Vector2 RandomVelocity(void) {
+    float speedX = (float)GetRandomValue(2, 6);
+    float speedY = (float)GetRandomValue(2, 6);
+    if (GetRandomValue(0, 1) == 0) speedX *= -1;
+    if (GetRandomValue(0, 1) == 0) speedY *= -1;
+    return (Vector2){ speedX, speedY };
+}
 
 int main(void) {
     SetConfigFlags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED
                  | FLAG_WINDOW_TOPMOST | FLAG_WINDOW_UNFOCUSED);
     InitWindow(800, 450, "Love Virus");
+    SetRandomSeed((unsigned int)time(NULL));
 
     int posX, posY, screenWidth, screenHeight;
     GetDesktopWorkArea(&posX, &posY, &screenWidth, &screenHeight);
@@ -15,26 +34,50 @@ int main(void) {
 
     SetTargetFPS(60);
 
-    Rectangle square = { 100, 100, 50, 50 };
-    Vector2 velocity = { 4.0f, 3.0f };
+    BouncingSquare squares[MAX_SQUARES];
+    int squareCount = 1;
+    squares[0].rect = (Rectangle){ 100, 100, SQUARE_SIZE, SQUARE_SIZE };
+    squares[0].velocity = (Vector2){ 4.0f, 3.0f };
 
     while (!WindowShouldClose()) {
-        square.x += velocity.x;
-        square.y += velocity.y;
-        if (square.x <= 0 || square.x + square.width >= screenWidth)   velocity.x *= -1;
-        if (square.y <= 0 || square.y + square.height >= screenHeight) velocity.y *= -1;
+        for (int i = 0; i < squareCount; i++) {
+            squares[i].rect.x += squares[i].velocity.x;
+            squares[i].rect.y += squares[i].velocity.y;
+
+            if (squares[i].rect.x <= 0 || squares[i].rect.x + squares[i].rect.width >= screenWidth)
+                squares[i].velocity.x *= -1;
+            if (squares[i].rect.y <= 0 || squares[i].rect.y + squares[i].rect.height >= screenHeight)
+                squares[i].velocity.y *= -1;
+        }
 
         int mouseX, mouseY;
         GetCursorPositionInWindow(windowHandle, &mouseX, &mouseY);
-        bool overSquare = CheckCollisionPointRec(
-            (Vector2){ (float)mouseX, (float)mouseY }, square);
+        Vector2 cursor = { (float)mouseX, (float)mouseY };
 
-        if (overSquare) ClearWindowState(FLAG_WINDOW_MOUSE_PASSTHROUGH);
-        else            SetWindowState(FLAG_WINDOW_MOUSE_PASSTHROUGH);
+        int hoveredIndex = -1;
+        for (int i = 0; i < squareCount; i++) {
+            if (CheckCollisionPointRec(cursor, squares[i].rect)) {
+                hoveredIndex = i;
+                break;
+            }
+        }
+
+        if (hoveredIndex != -1) ClearWindowState(FLAG_WINDOW_MOUSE_PASSTHROUGH);
+        else                    SetWindowState(FLAG_WINDOW_MOUSE_PASSTHROUGH);
+
+        if (hoveredIndex != -1 && IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && squareCount < MAX_SQUARES) {
+            squares[squareCount].rect = squares[hoveredIndex].rect;
+            squares[squareCount].velocity = RandomVelocity();
+            squareCount++;
+
+            squares[hoveredIndex].velocity = RandomVelocity();
+        }
 
         BeginDrawing();
             ClearBackground(BLANK);
-            DrawRectangleRec(square, RED);
+            for (int i = 0; i < squareCount; i++) {
+                DrawRectangleRec(squares[i].rect, RED);
+            }
         EndDrawing();
     }
 
